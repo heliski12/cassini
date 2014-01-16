fb2285473fecb71a821c775ff1aa19b94d29abb4
yottaram <yottaram@gmail.com>
2014-01-16 09:32:06 -0500

    text change to pre-auth landing page

--- app/views/pre-auth/landing.blade.php
+++ app/views/pre-auth/landing.blade.php
@@ -5,7 +5,7 @@
   <div class="container authorization">
     <div class="row">
       <div class="col-sm-12">
-        Thank you for signup up.  We will contact you shortly for access to the Marketplace.  <a href="{{ URL::to('/logout') }}">Log out</a>
+        Thank you for signup up.  We will contact you shortly for access.  <a href="{{ URL::to('/logout') }}">Log out</a>
       </div>
     </div>
   </div>
