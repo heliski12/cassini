c2c8f2e1e1dd0f416b893e6cd4debfe521171205
yottaram <yottaram@gmail.com>
2014-07-11 12:21:57 -0400

    increase asset version

--- app/config/cassini.php
+++ app/config/cassini.php
@@ -2,7 +2,7 @@
 
 return [
 
-  'asset_version' => '13',
+  'asset_version' => '14',
   'public_profile_launch_date' => new DateTime('2014-07-01'),
 
   'filestorage' => 's3', // 'filesystem',  // or s3
