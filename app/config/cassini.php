a9deef1c9e9f84d14006ca33a35c0fb5f3e23e87
jonstavis <jstavis@gmail.com>
2014-08-12 23:51:49 -0400

    increment asset version

--- app/config/cassini.php
+++ app/config/cassini.php
@@ -2,7 +2,7 @@
 
 return [
 
-  'asset_version' => '16',
+  'asset_version' => '17',
   'public_profile_launch_date' => new DateTime('2014-07-01'),
 
   'filestorage' => 's3', // 'filesystem',  // or s3
