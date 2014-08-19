11701d4863fd49bde081eb1200bffbb07b00f9a4
jonstavis <jstavis@gmail.com>
2014-08-19 10:15:36 -0400

    new public profiles page

--- app/views/profiles/show_public.blade.php
+++ app/views/profiles/show_public.blade.php
@@ -3,7 +3,9 @@
 <head>
     <title>Motionry Innovator: {{{ $profile->tech_title }}}</title>
 
-    {{ HTML::style('css/profile.css?' . Config::get('cassini.asset_version')) }}
+    {{ HTML::style('css/bootstrap.min.css') }}
+    {{ HTML::style('css/custom.css?'. Config::get('cassini.asset_version')) }}
+    {{ HTML::style('css/public-profile.css?' . Config::get('cassini.asset_version')) }}
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
 
     <meta property="og:image" content="{{{ $profile->public_image_url }}}">
@@ -38,6 +40,76 @@
 
 <body>
 
+
+
+    <header>
+      <nav class="navbar navbar-default" role="navigation">
+          <div class="navbar-header">
+            <a href="{{ URL::to('/') }}">
+              <img alt="Motionry Logo" class="logo" src="{{ asset('img/Black-Motionry-Logo.png') }}">
+            </a>
+          </div><!--/.navbar-header-->
+          <div class="nav-wrap">
+            <ul class="nav nav-pills navbar-right">
+              <li><a href="{{ URL::to('/#s') }}">Join Motionry</a></li>
+              <li><a href="{{ URL::to('/sign-in') }}">Sign In</a></li>
+                <li><a href="http://blog.motionry.com/">Blog</a></li>
+                <li><a class="twitter" href="https://twitter.com/motionry">Twitter</a></li>
+            </ul>
+          </div><!--/.nav-wrap -->
+      </nav>
+    </header>
+
+    
+    <div class="container group wrap">
+
+      
+
+
+      <div class="col-md-8 wrap">
+
+
+          <h3 class="col-xs-12 pitch">{{{ $profile->public_tagline_or_tech_title }}}</h3>
+
+
+      <div class="col-lg-6">
+       
+          {{ HTML::image($profile->public_image_url, $profile->public_image_description, [ 'class' => '', 'id' => 'company_img', 'width' => '100%' ]) }}
+        
+      </div>
+
+
+
+      <div class="col-lg-6">
+        <div class="heading-wrap">
+            <h3 id="company">{{{ $profile->organization_or_institution_name }}}</h3><a class="btn btn-xs btn-primary btn-profile" href="{{ route('show_profile', [ $profile->id ]) }}" role="button">View Full Profile</a>
+      </div>
+    
+      <p><strong>Technology Overview:</strong> {{{ $profile->public_description ?: "None specified." }}}</p>
+      <p><strong>Markets & Applications:</strong> {{{ $profile->sectors_tos }}}</p>
+        @foreach ($profile->applications as $application)
+            <div class="industry">{{{ $application->name }}}</div>
+        @endforeach
+      </div>
+
+    </div>
+
+    <aside class="col-md-3 col-md-offset-1">
+          <h4>Technology Transfer, Rebooted</h4>
+          <p>Discover and connect with the world&#039;s leading researchers, entrepreneurs and companies in energy, agriculture and material related technologies.</p> 
+          <a class="btn btn-sm btn-sign-up" href="{{ URL::to('/#s2') }}" role="button">Join Now for Free</a>
+      </aside>
+
+      
+
+    </div> <!-- /container -->
+
+    <footer>
+        <p>&copy; {{ date('Y') }} Motionry. All rights reserved.</p>
+      <footer>
+
+
+      {{--
 <div id="wrapper">
     <header>
     <a href="{{ URL::to('/') }}"><img alt="Motionry Logo" id="logo" src="{{ asset('img/Black-Motionry-Logo.png') }}"/></a>
@@ -75,7 +147,9 @@
     </aside>
 </div><!--end #wrapper -->
 
-<footer> Motionry &copy; {{ date('Y') }} </footer>
+<footer> Motionry &copy; {{ date('Y') }} </footer> 
+
+--}}
 
 </body>
 </html>
