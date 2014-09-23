e7ca46c2b98db0a6ade1a4733a93d389d1a998ea
jonstavis <jstavis@gmail.com>
2014-09-23 14:34:40 -0400

    increase margins on homepage

--- app/config/cassini.php
+++ app/config/cassini.php
@@ -2,7 +2,7 @@
 
 return [
 
-  'asset_version' => '24',
+  'asset_version' => '25',
   'public_profile_launch_date' => new DateTime('2014-07-01'),
 
   'filestorage' => 's3', // 'filesystem',  // or s3
