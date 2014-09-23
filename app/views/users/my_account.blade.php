4273faaa4b537666cd09244ccacea6b09684a093
jonstavis <jstavis@gmail.com>
2014-09-22 22:33:28 -0400

    my account v2 designs

--- app/views/users/my_account.blade.php
+++ app/views/users/my_account.blade.php
@@ -1,143 +1,122 @@
-@extends('layouts.private')
+@extends('layouts.master')
 
 @section('content')
-<div class="container-full">
-  <div class="my-account">
-    <div class="row">
-      <div class="col-md-4 col-md-offset-4">
-        <h4>My Motionry Account</h4>
-      </div> 
-    </div>
 
-    @if (Session::has('errors'))
-    <div class="row">
-      <div class="col-md-10 col-md-offset-1">
-        <div class="alert alert-danger">
-          {{ Session::get('errors')->first() }} 
-        </div>   
-      </div>
-    </div>
-    @endif
-    @if (Session::has('message'))
-    <div class="row">
-      <div class="col-md-10 col-md-offset-1">
-        <div class="alert alert-success">
-          {{ Session::get('message'); }} 
-        </div>   
-      </div>
-    </div>
-    @endif
+        <h1>My Motionry Account</h1>
 
-    <div class="my-account-inner">
-
-      @if (!empty($profiles) and sizeof($profiles) > 0)
+        @if (Session::has('errors'))
         <div class="row">
-          <div class="col-md-12">
-            <h5>Edit current profiles and add a person to edit profiles.  Note the person added must have an account.</h5>
+          <div class="col-md-10 col-md-offset-1">
+            <div class="alert alert-danger">
+              {{ Session::get('errors')->first() }} 
+            </div>   
           </div>
         </div>
+        @endif
+        @if (Session::has('message'))
+        <div class="row">
+          <div class="col-md-10 col-md-offset-1">
+            <div class="alert alert-success">
+              {{ Session::get('message'); }} 
+            </div>   
+          </div>
+        </div>
+        @endif
+
+        @if (!empty($profiles) and sizeof($profiles) > 0)
+        <p>Edit current profiles and add a person to edit profiles.  Note the person added must have an account.</p>
+
         @foreach ($profiles as $profile)
-          <div class="row my-profile">
-            <div class="col-md-2 col-sm-3 col-xs-12">
-                @if (!empty($profile->keypersons) and sizeof($profile->keypersons) > 0)
-                  <img class="marketplace-result-img" src="{{ asset($profile->keypersons[0]->photo->url('small')) }}"></img>
-                @else
-                  <img class="marketplace-result-img" src="{{ URL::to('/img/blank-avatar.jpg') }}"></img>
-                @endif
+          <div class="row top-buffer">
+
+            <div class=" col-xs-12 col-sm-2 ">
+              @if (!empty($profile->keypersons) and sizeof($profile->keypersons) > 0 and !empty($profile->keypersons[0]->photo_file_name))
+                <img class="marketplace-result-img" src="{{ asset($profile->keypersons[0]->photo->url('small')) }}"></img>
+              @else
+                <img class="marketplace-result-img" src="{{ URL::to('/img/blank-avatar.jpg') }}"></img>
+              @endif
             </div>
-            <div class="col-md-8 col-sm-9 col-xs-12">
+
+            <div class="col-xs-12 col-sm-8">
               @if (!empty($profile->keypersons) and sizeof($profile->keypersons) > 0)
                 {{ $profile->keypersons[0]->full_name }}<br/>
               @endif
-              <a href="{{ route('show_profile', [ 'id' => $profile->id ]) }}">{{ $profile->tech_title }}</a><br/>
               @if ($profile->innovator_type === 'RESEARCHER')
-                @if (!empty($profile->institution))
+                @if (!empty($profile->institution) && !empty($profile->institution->name))
                   {{ $profile->institution->name }}<br/>
                   {{ $profile->institution_department }}<br/>
                 @endif
-              @else
+              @elseif (!empty($profile->organization))
                 {{ $profile->organization }}<br/>
               @endif
               Status: {{ $profile->status_tos }}<br/>
               <a href="{{ route('edit_profile', [ 'id' => $profile->id ]) }}">Edit</a>&nbsp;&nbsp;|&nbsp;&nbsp;Secondary Editors:
-              <span class="secondary-editors">
+             <span class="secondary-editors">
                 @include('partials.secondary_editors', [ 'profile' => $profile ])
-              </span>
+             </span>
+
               {{ Form::open([ 'url' => route('add_editor'), 'class' => 'add-editor-form', 'role' => 'form' ]) }}
-                <div class="row">
-                  <div class="col-md-7 col-sm-9 col-xs-9">
-                    <div class="input-group add-secondary-editor">
-                      {{ Form::hidden('profile_id', $profile->id) }}
-                      {{ Form::text('email', null, [ 'class' => 'form-control input-sm', 'placeholder' => 'Email address of secondary editor...' ]) }}
-                      <span class="input-group-btn">
-                        {{ Form::submit('Add secondary editor', [ 'class' => 'btn btn-primary btn-sm' ]) }}
-                      </span>
-                    </div>
+              <div class="row">
+                <div class="col-md-7 col-sm-9 col-xs-9">
+                  <div class="input-group add-secondary-editor">
+                    {{ Form::hidden('profile_id', $profile->id) }}
+                    {{ Form::text('email', null, [ 'class' => 'form-control input-sm', 'placeholder' => 'Email address of secondary editor...' ]) }}
+                    <span class="input-group-btn">
+                      {{ Form::submit('Add secondary editor', [ 'class' => 'btn btn-primary btn-sm' ]) }}
+                    </span>
                   </div>
-                </div> 
-              {{ Form::close() }}
+                </div>
+              </div> 
+            {{ Form::close() }}
+
             </div>
-          </div>
+          </div><!--editor 1-->
         @endforeach
-      @else
-        <div class="row">
-          <div class="col-md-12">
+        @else
+          <div class="col-xs-12">
             @if (Auth::user()->innovator)
               <h5>You have not created any profiles.</h5>
             @else
               <h5>You have not been added as an editor on any profiles.</h5>
             @endif
           </div>
-        </div>
-      @endif
-      <div class="row">
-        <div class="col-md-12">
-          <hr/>
-        </div>
-      </div>
+        @endif
+
+       <hr/>
+
+
       @if (Auth::user()->innovator)
-        <div class="row">
-          <div class="col-md-12">
-            <h5>Add a new profile</h5>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-4 col-md-offset-2">
-            <a class="btn btn-primary" href="{{ URL::to('/profiles/new') }}">Create Profile</a>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-12">
-            <hr/>
-          </div>
-        </div>
+      <h5>Add a new profile</h5>
+
+      <p><a class="btn btn-primary" href="{{ URL::to('/profiles/new') }}">Create Profile</a></p>
+
+      <hr/>
       @endif
-      <div class="row">
-        <div class="col-md-12">
-          <h5>Change password (case sensitive and must be at least 8 characters in length)</h5>
+
+
+      <h5>Change password (case sensitive and must be at least 8 characters in length)</h5>
+
+<div class="row">
+  <div class="col-md-12">
+        {{ Form::open([ 'url' => route('update_password'), 'id' => 'update_password_form', 'role' => 'form', 'class' => 'form-horizontal' ]) }}
+     <div class="form-group">
+        {{ Form::label('old_password', 'Old Password', [ 'class' => 'col-md-2 control-label' ] ) }} 
+      <div class="col-md-4">
+        {{ Form::password('old_password') }}
+      </div>
+    </div>
+      <div class="form-group">
+        {{ Form::label('new_password', 'New Password', [ 'class' => 'col-md-2 control-label' ] ) }} 
+        <div class="col-md-4">
+          {{ Form::password('new_password') }}
         </div>
       </div>
-      <div class="row">
-        <div class="col-md-12">
-          {{ Form::open([ 'url' => route('update_password'), 'id' => 'update_password_form', 'role' => 'form', 'class' => 'form-horizontal' ]) }}
-          <div class="form-group">
-            {{ Form::label('old_password', 'Old Password', [ 'class' => 'col-md-2 control-label' ] ) }} 
-            <div class="col-md-4">
-              {{ Form::password('old_password') }}
-            </div>
+        <div class="form-group">
+          {{ Form::label('confirm_password', 'Confirm Password', [ 'class' => 'col-md-2 control-label' ] ) }} 
+          <div class="col-md-4">
+            {{ Form::password('confirm_password') }}
           </div>
-          <div class="form-group">
-            {{ Form::label('new_password', 'New Password', [ 'class' => 'col-md-2 control-label' ] ) }} 
-            <div class="col-md-4">
-              {{ Form::password('new_password') }}
-            </div>
-          </div>
-          <div class="form-group">
-            {{ Form::label('confirm_password', 'Confirm Password', [ 'class' => 'col-md-2 control-label' ] ) }} 
-            <div class="col-md-4">
-              {{ Form::password('confirm_password') }}
-            </div>
-          </div>
+        </div>
           <div class="form-group">
             <div class="col-md-offset-2 col-md-4">
               {{ Form::submit('Update', [ 'class' => 'btn btn-primary', 'id' => 'update_password', 'name' => 'submit' ]) }}
@@ -145,30 +124,17 @@
           </div>
           {{ Form::close() }}
         </div>
-      </div>
-      <div class="row">
-        <div class="col-md-12">
-          <hr/>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md-12">
-          <h5>Contact Motionry</h5>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md-offset-2 col-md-4">
-          <button class="btn btn-primary" data-toggle="modal" data-target="#contact">Email Motionry</button>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md-offset-2 col-md-4">
-          <h5>1-610-220-0184 (U.S.)</h5>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
+      </div> <!-- row -->
+
+        
+        <hr/>
+       
+       
+       <h5>Contact Motionry</h5>
+        
+      
+      <button class="btn btn-primary bottom-buffer" data-toggle="modal" data-target="#contact">Email Motionry</button>
+
 @stop
 
 @section('modal')
