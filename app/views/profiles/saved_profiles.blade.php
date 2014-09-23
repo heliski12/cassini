df72747cbf5f8df00371f6188962e3dee682e851
jonstavis <jstavis@gmail.com>
2014-09-22 21:34:07 -0400

    saved profiles v2 design

--- app/views/profiles/saved_profiles.blade.php
+++ app/views/profiles/saved_profiles.blade.php
@@ -1,8 +1,85 @@
-@extends('layouts.private')
+@extends('layouts.master')
 
 @section('content')
 
+      <h1 class="welcome">Saved Profiles</h1>
+        @if (Session::has('errors'))
+        <div class="row">
+          <div class="col-md-10 col-md-offset-1">
+            <div class="alert alert-danger">
+              {{ Session::get('errors')->first() }} 
+            </div>   
+          </div>
+        </div>
+        @endif
+        @if (Session::has('message'))
+        <div class="row">
+          <div class="col-md-10 col-md-offset-1">
+            <div class="alert alert-success">
+              {{ Session::get('message'); }} 
+            </div>   
+          </div>
+        </div>
+        @endif
 
+        @if (!empty($profiles) and sizeof($profiles) > 0)
+          <p>Click on box next to each profile and then Contact Motionry.  The team on the profile will contact you directly.</p>
+
+        <p>
+          <button class="btn btn-primary bottom-buffer saved-profile-contact" data-toggle="modal" data-target="#contact">Contact Motionry</button>
+        </p>
+
+        @foreach ($profiles as $profile)
+          <div class="panel panel-default my-profile">
+          <div class="panel-body">
+          <div class="panel-nav"></div>
+          
+          <div class="row saved-profile-controls">
+              <div class="col-xs-6 pull-left checkbox">
+                {{ Form::checkbox('select_'.$profile->id,'select_'.$profile->id, false, [ 'class' => 'contact-select', 'id' => $profile->id ]) }} 
+                {{ Form::hidden('id_'.$profile->id, $profile->id, [ 'id' => 'id_'.$profile->id ]) }} 
+                {{ Form::hidden('name_'.$profile->id, $profile->tech_title, [ 'id' => 'name_'.$profile->id ]) }} 
+              </div>
+              <div class="col-xs-6 pull-right remove">
+              <a href="#" class="icon remove-sp" pid="{{ $profile->id }}" title="Delete saved profile"><span class="glyphicon glyphicon-remove"></span></a>
+              </div>
+          </div>
+          <div class="row profile-content">
+            <div class="col-xs-4 col-sm-2">
+              <a href="{{ route('show_profile', [ 'id' => $profile->id ]) }}">
+                @if (!empty($profile->keypersons) and sizeof($profile->keypersons) > 0)
+                  <img class="my-profiles" src="{{ asset($profile->keypersons[0]->photo->url('small')) }}" />
+                @else
+                  <img class="my-profiles" src="{{ URL::to('/img/blank-avatar.jpg') }}" />
+                @endif
+              </a>
+            </div>
+            <div class="col-xs-8 col-sm-6">
+              @if (!empty($profile->keypersons) and sizeof($profile->keypersons) > 0)
+                {{ $profile->keypersons[0]->full_name }}<br/>
+              @endif
+              <a href="{{ route('show_profile', [ 'id' => $profile->id ]) }}">{{ $profile->tech_title }}</a><br/>
+              @if ($profile->innovator_type === 'RESEARCHER')
+                @if (!empty($profile->institution))
+                  {{ $profile->institution->name }}<br/>
+                  {{ $profile->institution_department }}<br/>
+                @endif
+              @else
+                {{ $profile->organization }}<br/>
+              @endif
+            </div>
+              
+          </div> <!-- profile-content -->
+        </div> <!-- panel-body -->
+        </div> <!-- panel-default -->
+      @endforeach
+    @else <!-- profiles -->
+      <div class="col-md-12">
+        <h5>You have not saved any profiles.</h5>
+      </div>
+    @endif <!-- profiles -->
+
+@if (false)
 <div class="container-full">
   <div class="my-account">
     <div class="row">
@@ -88,6 +165,7 @@
     </div>
   </div>
 </div>
+@endif
   
 @stop
 
