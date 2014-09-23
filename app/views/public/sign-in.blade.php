776169573eceb2d3e100ee2d15e8b37d548f649f
jonstavis <jstavis@gmail.com>
2014-09-22 20:16:51 -0400

    public sign-in page

--- app/views/public/sign-in.blade.php
+++ app/views/public/sign-in.blade.php
@@ -1,39 +1,49 @@
 @extends('layouts.public')
 
 @section('content')
+    <header>
+
+        <nav class="navbar navbar-default" role="navigation">
+          <div class="navbar-header">
+            <a href="{{ URL::to('/') }}">
+              <img alt="Motionry Logo" class="logo" src="{{URL::to('img/Black-Motionry-Logo.png')}}">
+            </a>
+          </div><!--/.navbar-header-->
+
+        </nav>
+      </header>
+
+      <hr class="horizontal-one">
+
   <div class="container">
-      <div class="row">
-          <div class="col-md-3 col-sm-6 col-xs-8">
-              <a href="{{ URL::to('/') }}"><img alt="Motionry Logo" class="logo img-responsive" src="{{ asset('img/Black-Motionry-Logo.png') }}"/></a>
-          </div>
-      </div>
-      <hr/>
 
       <div class="row">
           <div class="col-lg-4 col-lg-offset-4 col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 col-xs-12">
               <div class="signin">
+
                   <div class="login-or-signup">
                       <span class="login-msg">SIGN IN</span> or <span class="login-msg"><a href="#user_signup" data-toggle="modal" data-target="#user_signup">SIGN UP</a></span> 
                   </div>
-                  {{ Form::open([ 'url' => URL::to('/login'), 'id' => 'login_form' ]) }}
-                  @if (Session::has('status'))
-                  <div class="error-message">
-                      {{ Session::get('status') }} 
-                  </div>
-                  @endif
-                  {{ Form::text('email',null, [ 'placeholder' => 'Email address', 'class' => 'form-control' ]) }}
-                  {{ Form::password('password', [ 'placeholder' => 'Password', 'class' => 'form-control' ]) }}
-                  <a class="forgot" href="#" data-toggle='modal' data-target="#forgot_password">Forgot password?</a>
 
-                  {{ Form::button('SIGN IN', [ 'class' => 'btn btn-primary', 'type' => 'submit' ]) }}
+                  {{ Form::open([ 'url' => URL::to('/login'), 'id' => 'login_form', 'class' => 'top-buffer' ]) }}
+                    @if (Session::has('status'))
+                    <div class="error-message">
+                        {{ Session::get('status') }} 
+                    </div>
+                    @endif
 
+                    {{ Form::text('email',null, [ 'placeholder' => 'Email address', 'class' => 'form-control' ]) }}
+                    {{ Form::password('password', [ 'placeholder' => 'Password', 'class' => 'top-buffer form-control' ]) }}
+
+                    <a class="forgot top-buffer" href="#" data-toggle='modal' data-target="#forgot_password">Forgot password?</a>
+
+                    {{ Form::button('SIGN IN', [ 'class' => 'btn btn-primary top-buffer left-buffer', 'type' => 'submit' ]) }}
                   {{ Form::close() }}
-              </div>
-          </div>
+                </div> <!-- signin -->
+            </div> <!-- col-lg-4 -->
 
-      </div>
-  </div>
-
+        </div> <!-- row -->
+    </div> <!-- container -->
 @stop
 
 @section('modal')
--- public/js/initializer.js
+++ public/js/initializer.js
@@ -28,6 +28,15 @@ $(function(){
     });
   });
 
+  $(document).on('submit','#password_reminder_form', function(event) {
+    event.preventDefault();
+    $.post($(this).attr('action'), 
+      $(this).serialize(),
+      function(data, textStatus, request) {
+        $('#forgot_password_modal_content').html(data);
+    });
+  });
+
   if (window.location.hash != undefined && (window.location.hash == '#s' || window.location.hash == '#s2')) {
       $("#user_signup").modal();
   }
