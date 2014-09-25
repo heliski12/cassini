678f61c4121bc0756bba1aeedcc00c8d00a558d6
jonstavis <jstavis@gmail.com>
2014-09-25 14:17:20 -0400

    add profile title back to my account

--- app/views/users/my_account.blade.php
+++ app/views/users/my_account.blade.php
@@ -38,7 +38,10 @@
             </div>
 
             <div class="col-xs-12 col-sm-8">
-              @if (!empty($profile->keypersons) and sizeof($profile->keypersons) > 0)
+              @if (!empty($profile->tech_title))
+                  <h4><a href="{{ route('show_profile', [ $profile->id ]) }}">{{ $profile->tech_title }}</a></h4>
+              @endif
+              @if (!empty($profile->keypersons) and sizeof($profile->keypersons) > 0 and trim($profile->keypersons[0]->full_name))
                 {{ $profile->keypersons[0]->full_name }}<br/>
               @endif
               @if ($profile->innovator_type === 'RESEARCHER')
